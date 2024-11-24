1fbf20917a94fbf999a87aff4c8d5670a1cabb8e
Marcos Gabriel <marcosggomes27@gmail.com>
2024-11-24 20:37:23 -0300

    adiciona imprime remove e libera

--- main.c
+++ main.c
@@ -1,9 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "tad-lista.h"
+
+struct lista {
+	int info;
+	Lista* p;
+};
 
 int main(int argc, char *argv[]) {
 	
-	printf("Hello sexo!");
+	Lista* l = cria_lista();
+	l = adiciona_elemento_lista(l, 1);
+	l = adiciona_elemento_lista(l, 2);
+	l = adiciona_elemento_lista(l, 3);
+	l = adiciona_elemento_lista(l, 4);
+	l = adiciona_elemento_lista(l, 5);
+	
+	printf("Imprimindo primeira vez: ");
+	imprime_lista(l);
+	printf("\n");
+	
+	l = retira_elemento_lista(l, 4);
+	imprime_elemento_lista(l, 2);
+	printf("Imprimindo segunda vez: ");
+	imprime_lista(l);
+	printf("\n");
+	
+	libera_lista(l);
+	
+	l = cria_lista();
+	l = adiciona_elemento_lista(l, 10);
+	printf("Imprimindo terceira vez: ");
+	imprime_lista(l);
+	printf("\n");
+	
+	system("PAUSE");
 	return 0;
 	
 }
--- tad-lista.c
+++ tad-lista.c
@@ -7,7 +7,7 @@ struct lista {
 	Lista* p;
 };
 
-Lista* cria_lista(Lista*l){
+Lista* cria_lista(){
 	Lista* laux = NULL;
 	return laux;
 }
@@ -16,4 +16,81 @@ int lista_vazia(Lista*l){
 	return l == NULL;
 }
 
+Lista* adiciona_elemento_lista(Lista* l, int info){
+	Lista* laux = (Lista*)malloc(sizeof(Lista));
+	if(laux == NULL){
+		printf("Memoria insuficiente\n");
+		exit(1);
+	}
+	laux->info = info;
+	laux->p = l;
+	return laux;
+}
 
+void imprime_elemento_lista(Lista* l, int info){
+	if(lista_vazia(l)){
+		printf("Lista vazia\n");
+		exit(1);
+	}
+	Lista* laux = l;
+	while(laux != NULL){
+		if(laux->info == info){
+			printf("Elemento %d = %d\n", info, laux->info);
+			return;
+		}
+		laux = laux->p;
+	}
+	printf("Elemento nao ta na lista\n");
+}
+
+void imprime_lista(Lista* l){
+	if(lista_vazia(l)){
+		printf("Lista vazia\n");
+		exit(1);
+	}
+	Lista* laux = l;
+	while(laux != NULL){
+		printf("%d - ", laux->info);
+		laux = laux->p;
+	}
+}
+
+Lista* retira_elemento_lista(Lista* l, int info){
+	if(lista_vazia(l)){
+		printf("Lista vazia\n");
+		exit(1);
+	}
+	Lista* lant = NULL;
+	Lista* laux = l;
+	while(laux != NULL){
+		if(laux->info == info){
+			if(lant == NULL){
+				l = laux->p;
+				free(laux);
+				return l;
+			}
+			if(laux->p == NULL){
+				lant->p = laux->p;
+				free(laux);
+				return l;
+			}
+			lant->p = laux->p;
+			free(laux);
+			return l;
+		}else{
+			lant = laux;
+			laux = laux->p;
+		}
+	}
+}
+
+void libera_lista(Lista* l){
+	if(l!= NULL){
+		Lista* lprox;
+		while(l != NULL){
+			lprox = l->p;
+			free(l);
+			l = lprox;
+		}
+	}
+}
